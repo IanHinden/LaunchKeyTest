8eb1a3c8b560ab4bc97af5bae96e1aabc112c0fd
IanHinden <Ian.Hinden@gmail.com>
2018-01-23 09:17:52 -0800

    Added Debug / Linked Index Template

--- helloflask.py
+++ helloflask.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Welcometolaunchkey"
+app.debug = True
+
+class LoginForm(FlaskForm):
+	username = StringField("username", validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField("password", validators=[InputRequired(), Length(min=8)])
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+	form = LoginForm()
+	
+	return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run()
